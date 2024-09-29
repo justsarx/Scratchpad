4a3d870565af318bebf7407f8196bae54ba0581a
justsarx <sinhasarthak56@gmail.com>
2024-09-29 23:03:16 +0530

    Add project files.

---
+++ Notepad/Form1.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        EditToolStripMenuItem = New ToolStripMenuItem()
+        CutToolStripMenuItem = New ToolStripMenuItem()
+        CopyToolStripMenuItem = New ToolStripMenuItem()
+        PasteToolStripMenuItem = New ToolStripMenuItem()
+        FontToolStripMenuItem = New ToolStripMenuItem()
+        ViewToolStripMenuItem = New ToolStripMenuItem()
+        ZoomInToolStripMenuItem = New ToolStripMenuItem()
+        ZoomOutToolStripMenuItem = New ToolStripMenuItem()
+        RichTextBox1 = New RichTextBox()
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        FontDialog1 = New FontDialog()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, EditToolStripMenuItem, ViewToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, SaveToolStripMenuItem, SaveAsToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Image = CType(resources.GetObject("NewToolStripMenuItem.Image"), Image)
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.N
+        NewToolStripMenuItem.Size = New Size(186, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Image = CType(resources.GetObject("OpenToolStripMenuItem.Image"), Image)
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.O
+        OpenToolStripMenuItem.Size = New Size(186, 22)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Image = CType(resources.GetObject("SaveToolStripMenuItem.Image"), Image)
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.S
+        SaveToolStripMenuItem.Size = New Size(186, 22)
+        SaveToolStripMenuItem.Text = "Save "
+        ' 
+        ' SaveAsToolStripMenuItem
+        ' 
+        SaveAsToolStripMenuItem.Image = CType(resources.GetObject("SaveAsToolStripMenuItem.Image"), Image)
+        SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        SaveAsToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Shift Or Keys.S
+        SaveAsToolStripMenuItem.Size = New Size(186, 22)
+        SaveAsToolStripMenuItem.Text = "Save As"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Image = CType(resources.GetObject("ExitToolStripMenuItem.Image"), Image)
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Q
+        ExitToolStripMenuItem.Size = New Size(186, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' EditToolStripMenuItem
+        ' 
+        EditToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CutToolStripMenuItem, CopyToolStripMenuItem, PasteToolStripMenuItem, FontToolStripMenuItem})
+        EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        EditToolStripMenuItem.Size = New Size(39, 20)
+        EditToolStripMenuItem.Text = "Edit"
+        ' 
+        ' CutToolStripMenuItem
+        ' 
+        CutToolStripMenuItem.Image = CType(resources.GetObject("CutToolStripMenuItem.Image"), Image)
+        CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        CutToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.X
+        CutToolStripMenuItem.Size = New Size(161, 22)
+        CutToolStripMenuItem.Text = "Cut"
+        ' 
+        ' CopyToolStripMenuItem
+        ' 
+        CopyToolStripMenuItem.Image = CType(resources.GetObject("CopyToolStripMenuItem.Image"), Image)
+        CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        CopyToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.C
+        CopyToolStripMenuItem.Size = New Size(161, 22)
+        CopyToolStripMenuItem.Text = "Copy"
+        ' 
+        ' PasteToolStripMenuItem
+        ' 
+        PasteToolStripMenuItem.Image = CType(resources.GetObject("PasteToolStripMenuItem.Image"), Image)
+        PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        PasteToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.V
+        PasteToolStripMenuItem.Size = New Size(161, 22)
+        PasteToolStripMenuItem.Text = "Paste"
+        ' 
+        ' FontToolStripMenuItem
+        ' 
+        FontToolStripMenuItem.Image = CType(resources.GetObject("FontToolStripMenuItem.Image"), Image)
+        FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        FontToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Alt Or Keys.F
+        FontToolStripMenuItem.Size = New Size(161, 22)
+        FontToolStripMenuItem.Text = "Font"
+        ' 
+        ' ViewToolStripMenuItem
+        ' 
+        ViewToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ZoomInToolStripMenuItem, ZoomOutToolStripMenuItem})
+        ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        ViewToolStripMenuItem.Size = New Size(44, 20)
+        ViewToolStripMenuItem.Text = "View"
+        ' 
+        ' ZoomInToolStripMenuItem
+        ' 
+        ZoomInToolStripMenuItem.Image = CType(resources.GetObject("ZoomInToolStripMenuItem.Image"), Image)
+        ZoomInToolStripMenuItem.Name = "ZoomInToolStripMenuItem"
+        ZoomInToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Up
+        ZoomInToolStripMenuItem.Size = New Size(194, 22)
+        ZoomInToolStripMenuItem.Text = "Zoom In"
+        ' 
+        ' ZoomOutToolStripMenuItem
+        ' 
+        ZoomOutToolStripMenuItem.Image = CType(resources.GetObject("ZoomOutToolStripMenuItem.Image"), Image)
+        ZoomOutToolStripMenuItem.Name = "ZoomOutToolStripMenuItem"
+        ZoomOutToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Down
+        ZoomOutToolStripMenuItem.Size = New Size(194, 22)
+        ZoomOutToolStripMenuItem.Text = "Zoom Out"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.AcceptsTab = True
+        RichTextBox1.BackColor = SystemColors.Window
+        RichTextBox1.BorderStyle = BorderStyle.None
+        RichTextBox1.Dock = DockStyle.Fill
+        RichTextBox1.EnableAutoDragDrop = True
+        RichTextBox1.Location = New Point(0, 24)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.ScrollBars = RichTextBoxScrollBars.ForcedBoth
+        RichTextBox1.Size = New Size(800, 426)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.DefaultExt = "txt"
+        OpenFileDialog1.Filter = "Text Files|*.txt|All Files|"
+        ' 
+        ' SaveFileDialog1
+        ' 
+        SaveFileDialog1.DefaultExt = "txt"
+        SaveFileDialog1.Filter = "Text Files|*.txt|All Files|"
+        ' 
+        ' FontDialog1
+        ' 
+        FontDialog1.Color = Color.White
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Desktop
+        ClientSize = New Size(800, 450)
+        Controls.Add(RichTextBox1)
+        Controls.Add(MenuStrip1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Scratchpad"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZoomInToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZoomOutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents FontDialog1 As FontDialog
+
+End Class
---
+++ Notepad/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+
+Public Class Form1
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Dim newresult As MsgBoxResult
+        newresult = MsgBox("Clear Buffer ?", vbOKCancel + vbExclamation, "New")
+        If newresult = vbOK Then
+            Me.Text = "Scratchpad - Untitled"
+            RichTextBox1.Text = ""
+        Else
+            Return
+        End If
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim openresult As DialogResult = OpenFileDialog1.ShowDialog()
+        If openresult = DialogResult.OK Then
+            RichTextBox1.Text = ""
+            Using sr As StreamReader = New StreamReader(OpenFileDialog1.FileName)
+                RichTextBox1.Text = sr.ReadToEnd
+            End Using
+            Me.Text = Format("Scratchpad - " + OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        If SaveFileDialog1.FileName = "" Then
+            SaveAsToolStripMenuItem_Click(sender, e)
+        Else
+            Using sr As New StreamWriter(SaveFileDialog1.FileName)
+                sr.WriteLine(RichTextBox1.Text)
+            End Using
+        End If
+
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        Dim saveresult As DialogResult = SaveFileDialog1.ShowDialog()
+        If saveresult = DialogResult.OK Then
+            Using sr As New StreamWriter(SaveFileDialog1.FileName)
+                sr.WriteLine(RichTextBox1.Text)
+            End Using
+            Me.Text = Format("Scratchpad - " + SaveFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        If SaveFileDialog1.FileName = "" Then
+            Dim exitresult As MsgBoxResult
+            exitresult = MsgBox("Quit without saving ?", vbYesNoCancel + vbExclamation, "Exit")
+            If exitresult = MsgBoxResult.Cancel Then
+                Return
+            ElseIf exitresult = MsgBoxResult.Yes Then
+                Me.Close()
+            ElseIf exitresult = MsgBoxResult.No Then
+                SaveAsToolStripMenuItem_Click(sender, e)
+            End If
+        Else
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        RichTextBox1.Cut()
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        RichTextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        RichTextBox1.Paste()
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        Dim fontresult As DialogResult
+        fontresult = FontDialog1.ShowDialog()
+        If fontresult = DialogResult.OK Then
+            RichTextBox1.Font = FontDialog1.Font
+        End If
+    End Sub
+
+    Private Sub ZoomInToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ZoomInToolStripMenuItem.Click
+        RichTextBox1.Font = New Font(RichTextBox1.Font.FontFamily, RichTextBox1.Font.Size + 1, RichTextBox1.Font.Style)
+    End Sub
+
+    Private Sub ZoomOutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ZoomOutToolStripMenuItem.Click
+        RichTextBox1.Font = New Font(RichTextBox1.Font.FontFamily, RichTextBox1.Font.Size - 1, RichTextBox1.Font.Style)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Scratchpad - Untitled"
+    End Sub
+End Class
